c98f1116b04170832b6e5ec1bbc9378c54312adc
Michal Omri <michal_omri@yahoo.com>
2025-07-01 22:38:19 +0300

    Set up NoteIndex component structure and basic logic

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,68 @@
+import { noteService } from '../services/note.service.js'
+// import { NoteFilter } from "../cmps/NoteFilter.jsx"
+// import { NoteList } from "../cmps/NoteList.jsx"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { getTruthyValues } from "../services/util.service.js"
+
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
+
 
 export function NoteIndex() {
-    return <section className="container">Notes app</section>
+    const [notes, setNotes] = useState(null)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [filterBy, setFilterBy] = useState(noteService.getFilterFromSearchParams(searchParams))//if it's in url it's seen in the input and filtered results (one way data binding)
+
+    useEffect(() => {
+        loadNotes()
+        setSearchParams(getTruthyValues(filterBy)) //two-way data binding. whatever is typed in input filer is copied to the url
+    }, [filterBy])
+
+    function loadNotes() {
+        noteService.query(filterBy)
+            .then(notes => setNotes(notes))
+            .catch(err => console.log('err:', err))
+    }
+
+    function onRemoveNote(noteId) {
+        noteService.remove(noteId)
+            .then(() => {
+                showSuccessMsg('Note was removed successfully!')
+                setNotes(notes => notes.filter(note => note.id !== noteId))
+            })
+            .catch(err => {
+                console.log(err)
+                showErrorMsg('Problem removing note')
+            })
+    }
+
+    function onSetFilter(filterBy) {
+        setFilterBy(prevFilter => ({ ...prevFilter, ...filterBy }))
+    }
+
+    if (!notes) return <div>Loading...</div>
+
+    return (
+        <section className="container">Notes app
+
+            {/* <section className="note-index">
+                <NoteFilter
+                    defaultFilter={filterBy}
+                    onSetFilter={onSetFilter}
+                />
+
+                <NoteList
+                    notes={notes}
+                    onRemoveNote={onRemoveNote}
+                />
+            </section> */}
+        </section>
+    )
+
 }
+
+
+
+
+
+
